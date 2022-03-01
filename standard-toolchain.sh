9663a72126015226caa221d86fe6f55f47ac8724
0xvpr <malikrb@live.com>
2022-03-01 15:27:54 +0000

    toolchain improved; vim setup improved

--- standard-toolchain.sh
+++ standard-toolchain.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
 # Creator: VPR
-# Created: December 7, 2021
-# Updated: December 7, 2021
+# Created: December 7th, 2021
+# Updated: March 1st, 2021
 
 set -o pipefail
 set -o errexit
 set -o nounset
 set -o xtrace
 
+# Set timezone to avoid input requirements
+export TZ=America/Chicago
+ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
+
 apt update && apt upgrade -y
 apt install -y --no-install-recommends \
+                                       sudo \
+                                       apt-utils \
                                        zsh \
                                        zsh-autosuggestions \
+                                       tmux \
                                        git \
                                        curl \
                                        wget \
@@ -23,16 +30,31 @@ apt install -y --no-install-recommends \
                                        neovim \
                                        tree \
                                        zip \
-                                       build-essential \
                                        mingw-w64 \
                                        mingw-w64-common \
                                        mingw-w64-i686-dev \
                                        mingw-w64-x86-64-dev \
-                                       clang \
-                                       gcc \
+                                       build-essential \
+                                       pkg-config \
                                        gdb \
+                                       gcc \
+                                       llvm \
+                                       clang \
+                                       clangd \
+                                       cmake \
                                        cscope \
                                        python2-dev \
                                        python3-dev \
                                        nodejs \
-                                       yarn 
+                                       yarnpkg 
+
+# Copy config files
+cp config/.tmux.conf ~/
+cp config/.zshrc ~/
+cp config/.utils ~/
+
+# Install oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# Install p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
--- vim-setup.sh
+++ vim-setup.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
 # Creator: VPR
-# Created: December 7, 2021
-# Updated: December 7, 2021
+# Created: December 7th, 2021
+# Updated: March 1st, 2022
 
 set -o pipefail
 set -o errexit
 set -o nounset
 set -o xtrace
 
+if [ -f ~/.vimrc ]
+then
+    mv ~/.vimrc ~/.vimrc.bak
+fi
+
+cp .vimrc ~/.vimrc
 cp .vim ~/.vim -r
 
 mkdir -p ~/.vim/bundle/
 
-git clone https://github.com/neoclide/coc.nvim ~/.vim/bundle/coc.nvim
+(
+    git clone https://github.com/neoclide/coc.nvim ~/.vim/bundle/coc.nvim
+    cd ~/.vim/bundle/coc.nvim && yarnpkg install
+)
+
 git clone https://github.com/preservim/nerdcommenter ~/.vim/bundle/nerdcommenter
 git clone https://github.com/preservim/nerdtree ~/.vim/bundle/nerdtree
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
