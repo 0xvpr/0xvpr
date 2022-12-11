4f0d6548a5f3691923e8372f2908da60ad21bf3d
0xvpr <malikrb@live.com>
2022-12-10 23:03:11 -0500

    vim-setup improved

--- profile-setup.sh
+++ profile-setup.sh
@@ -6,13 +6,13 @@
 # Updated by:   VPR
 # Updated:      December 10, 2022
 
-# Install oh-my-zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
-# Install p10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 # Copy config files
 cp config/.tmux.conf ~/
 cp config/.zshrc ~/
 cp -r config/.utils ~/
+
+# Install p10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
--- vim-setup.sh
+++ vim-setup.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
 
-# Creator: VPR
-# Created: December 7th, 2021
-# Updated: March 1st, 2022
+# Created by:   VPR
+# Created:      December 7th, 2021
+
+# Updated by:   VPR
+# Updated:      December 10th, 2022
+
 
 set -o pipefail
 set -o errexit
 set -o nounset
 set -o xtrace
 
-if [ -f ~/.vimrc ]
-then
-    mv ~/.vimrc ~/.vimrc.bak
-fi
-
-# Initialize ~/.vim
-cp config/.vim ~/.vim -r
-cp config/.vimrc ~/.vimrc
-mkdir -p ~/.vim/bundle/
-
-# Add and link nvim config to ~/.vimrc
-mkdir -p ~/.config
-cp config/nvim ~/.config -r
-
-# Install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-# Build and install COC
 (
-    git clone https://github.com/neoclide/coc.nvim ~/.vim/bundle/coc.nvim
-    cd ~/.vim/bundle/coc.nvim && yarnpkg install
-)
+    # Set script directory as cwd
+    cd "${0%/*}"
 
-# Clone additional resources
-git clone https://github.com/preservim/nerdcommenter ~/.vim/bundle/nerdcommenter
-git clone https://github.com/preservim/nerdtree ~/.vim/bundle/nerdtree
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+    # Make a copy of the old vimrc with timestamp
+    if [ -f ~/.vimrc ]
+    then
+        mv ~/.vimrc ~/.vimrc_`date '+%Y-%m-%d_%H-%M-%S'`
+    fi
+
+    # Initialize ~/.vim
+    cp ./config/.vim ~/.vim -r
+    ln -s $PWD/.vimrc ~/.vimrc
+    mkdir -p ~/.vim/bundle/
+
+    # Add and link nvim config to ~/.vimrc
+    mkdir -p ~/.config
+    cp ./config/nvim ~/.config -r
+
+    # Install pathogen
+    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+    # Build and install COC
+    (
+        git clone https://github.com/neoclide/coc.nvim ~/.vim/bundle/coc.nvim
+        cd ~/.vim/bundle/coc.nvim && yarnpkg install
+    )
+
+    # Clone additional resources
+    git clone https://github.com/preservim/nerdcommenter ~/.vim/bundle/nerdcommenter
+    git clone https://github.com/preservim/nerdtree ~/.vim/bundle/nerdtree
+    git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+    git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+
+)
