6e7a3abf623797b5231c936b679cff8619bdfec9
VPR <>
2025-08-08 16:04:06 -0400

    deshitify install (some) scripts

--- install/standard-toolchain.sh
+++ install/standard-toolchain.sh
@@ -1,8 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Creator:    VPR
-# Created:    December 7th, 2021
-# Updated:    June 11th, 2022
+# Created by:   VPR
+# Created:      December 7th, 2021
+#
+# Updated by:   VPR
+# Updated:      August 8th, 2025
 
 set -o pipefail
 set -o errexit
@@ -10,41 +12,46 @@ set -o nounset
 set -o xtrace
 
 # Set timezone to avoid input requirements
-export TZ=America/Chicago
-ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
+TZ=America/New_York
+[[ $(diff "/usr/share/zoneinfo/${TZ}" /etc/localtime) ]] || sudo ln -s /usr/share/zoneinfo/${TZ} /etc/localtime && sudo echo ${TZ} > /etc/timezone
 
 # Install a high enough node version for nvim
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+[[ $(which nodejs) ]] || curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
 
 # Update repositories
-sudo apt update
+sudo apt update && sudo apt upgrade -y --no-install-recommends
 sudo apt install -y --no-install-recommends \
-    sudo \
-    apt-utils \
-    zsh \
-    zsh-autosuggestions \
-    tmux \
-    git \
-    curl \
-    wget \
-    htop \
-    iftop \
-    iotop \
-    vim \
-    neovim \
-    tree \
-    zip \
-    mingw-w64 \
-    mingw-w64-common \
-    mingw-w64-i686-dev \
-    mingw-w64-x86-64-dev \
-    build-essential \
-    pkg-config \
-    gdb \
-    gcc \
-    cmake \
-    cscope \
-    python2-dev \
-    python3-dev \
-    nodejs \
-    yarnpkg 
+  sudo \
+  apt-utils \
+  zsh \
+  zsh-autosuggestions \
+  tmux \
+  git \
+  curl \
+  wget \
+  htop \
+  iftop \
+  iotop \
+  vim \
+  neovim \
+  tree \
+  zip \
+  net-tools \
+  mingw-w64 \
+  mingw-w64-common \
+  mingw-w64-i686-dev \
+  mingw-w64-x86-64-dev \
+  build-essential \
+  pkg-config \
+  gdb \
+  gcc \
+  cmake \
+  cscope \
+  python3-dev \
+  nodejs
+
+# Get yarn for vim
+[[ $(which cmdtest) ]] || sudo apt remove --purge cmdtest -y
+sudo corepack enable
+sudo corepack prepare yarn@stable --activate
+echo 'Y' | yarn -v 
--- install/vim-setup.sh
+++ install/vim-setup.sh
@@ -4,7 +4,7 @@
 # Created:      December 7th, 2021
 
 # Updated by:   VPR
-# Updated:      May 2nd, 2024
+# Updated:      August 8th, 2025
 
 
 set -o pipefail
@@ -18,36 +18,42 @@ set -o xtrace
     cd "${0%/*}"
 
     # Make a copy of the old vimrc with timestamp
-    if [ -f ~/.vimrc ]
+    if ! [[ -f ~/.vimrc ]]
     then
         cp -r ~/.vimrc ~/.vimrc_`date '+%Y-%m-%d_%H-%M-%S'`
     fi
 
     # Initialize ~/.vim
-    if [ -d ~/.vim ]
+    if ! [[ -d ~/.vim ]]
     then
         cp -r ~/.vim ~/.vim_`date '+%Y-%m-%d_%H-%M-%S'`
     fi
 
     # Add and link nvim config to ~/.vimrc
     mkdir -p ~/.config
-    cp -r ../.config/nvim ~/.config/
-    cp -r ../.vim/ ~/.vim/
-    cp -r ../.vimrc ~/.vimrc
+    [[ -d ~/.config ]] || cp -r ../.config/nvim ~/.config/
+    [[ -d ~/.vim    ]] || cp -r ../.vim/ ~/.vim/
+    [[ -e ~/.vimrc  ]] || cp -r ../.vimrc ~/.vimrc
 
     # Install pathogen
-    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    if ! [[ -e ~/.vim/autoload/pathogen.vim ]] 
+    then
+        (
+            mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+            curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+        )
+    fi
 
     # Build and install COC
-    (
-        git clone https://github.com/neoclide/coc.nvim ~/.vim/bundle/coc.nvim
-        cd ~/.vim/bundle/coc.nvim && yarn install
-    )
+    [[ -d ~/.vim/bundle/coc.nvim ]] || git clone https://github.com/neoclide/coc.nvim ~/.vim/bundle/coc.nvim
+    if [[ -d "${HOME}/.config/coc" ]]
+    then
+        [[ $(which yarn) ]] && ( cd ~/.vim/bundle/coc.nvim && yarn )
+    fi
 
     # Clone additional resources
-    git clone https://github.com/preservim/nerdcommenter ~/.vim/bundle/nerdcommenter
-    git clone https://github.com/preservim/nerdtree ~/.vim/bundle/nerdtree
-    git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-    git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+    [[ -d ~/.vim/bundle/nerdcommenter      ]] || git clone https://github.com/preservim/nerdcommenter        ~/.vim/bundle/nerdcommenter
+    [[ -d ~/.vim/bundle/nerdtree           ]] || git clone https://github.com/preservim/nerdtree             ~/.vim/bundle/nerdtree
+    [[ -d ~/.vim/bundle/vim-airline        ]] || git clone https://github.com/vim-airline/vim-airline        ~/.vim/bundle/vim-airline
+    [[ -d ~/.vim/bundle/vim-airline-themes ]] || git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
 )
